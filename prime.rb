3c7e659500934373814d67b852197e03be417dbd
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-16 05:01:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
-def prime?(num)
-   for x in 2..num-1
-      print x
-      if num % x == 0
-         return false
-      else
-         return true
-      end
-   end
-end 
+def prime?(number)
+     start = 2
+		 if number > 1
+		 range = (start..number-1).to_a
+		 range.none? do |num_to_test|
+		 number % num_to_test == 0
+end
+     else false
+end
+end
